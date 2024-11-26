e2e76450ee5b40ad3035e1b28d521cc051f0149a
manfredi.marceca <manfredi.marceca@outlook.it>
2024-11-26 23:08:52 +0100

    First draft of Docx to Markdown converter

---
+++ src/samples/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.IO;
+using DocSharp.Docx;
+using Microsoft.Win32;
+
+namespace WpfApp1;
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void DocxToMdTest_Click(object sender, RoutedEventArgs e)
+    {
+        var ofd = new OpenFileDialog()
+        {
+            Filter = "Word OpenXML document|*.docx",
+            Multiselect = false,
+        };
+        if (ofd.ShowDialog(this) == true)
+        {
+            var sfd = new SaveFileDialog()
+            {
+                Filter = "Markdown|*.md;*.markdown",
+                FileName = Path.GetFileNameWithoutExtension(ofd.FileName) + ".md"
+            };
+            if (sfd.ShowDialog(this) == true)
+            {
+                DocxToMdConverter.DocxToMarkdown(ofd.FileName, sfd.FileName);
+            }
+        }
+    }
+}
