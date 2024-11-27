6dc0825db969882fe453b627981a11f499d72a64
manfredi.marceca <manfredi.marceca@outlook.it>
2024-11-27 21:55:32 +0100

    Restructure docx project to be extensible (still more to do)

---
+++ src/DocSharp.Docx/DocxConverter.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Wordprocessing;
+
+namespace DocSharp.Docx;
+
+public abstract class DocxConverterBase
+{
+    public string ConvertToString(Stream inputStream)
+    {
+        using (var wordDocument = WordprocessingDocument.Open(inputStream, false))
+        {
+            var sb = new StringBuilder();
+            var body = wordDocument.MainDocumentPart?.Document.Body;
+            if (body != null)
+            {
+                foreach (var element in body.Elements())
+                {
+                    ProcessBodyElement(element, sb);                                      
+                }
+            }
+            return sb.ToString();
+        }
+    }
+
+    public string ConvertToString(string inputFilePath)
+    {
+        using (var fileStream = new FileStream(inputFilePath, FileMode.Open, FileAccess.Read))
+        {
+            return ConvertToString(fileStream);
+        }
+    }
+
+    public void Convert(string inputFilePath, string outputFilePath)
+    {
+        File.WriteAllText(outputFilePath, ConvertToString(inputFilePath));
+    }
+
+    public void Convert(Stream inputStream, string outputStream)
+    {
+        using (var streamWriter = new StreamWriter(outputStream))
+        {
+            streamWriter.Write(ConvertToString(inputStream));
+        }
+    }
+
+    internal virtual void ProcessBodyElement(OpenXmlElement element, StringBuilder sb)
+    {
+        switch (element)
+        {
+            case Paragraph paragraph:
+                ProcessParagraph(paragraph, sb);
+                break;           
+            case Table table:
+                ProcessTable(table, sb);
+                break;
+        }
+    }
+
+    internal virtual void ProcessParagraph(Paragraph paragraph, StringBuilder sb)
+    {
+        foreach(var element in paragraph.Elements())
+        {
+            switch (element)
+            {
+                case Run run:
+                    ProcessRun(run, sb);
+                    break;
+                case Hyperlink hyperlink:
+                    ProcessHyperlink(hyperlink, sb);
+                    break;
+                case Picture picture:
+                    ProcessPicture(picture, sb);
+                    break;
+            }
+        }
+    }
+
+    internal abstract void ProcessTable(Table table, StringBuilder sb);
+
+    internal abstract void ProcessRun(Run run, StringBuilder sb);
+
+    internal abstract void ProcessPicture(Picture picture, StringBuilder sb);
+
+    internal abstract void ProcessHyperlink(Hyperlink hyperlink, StringBuilder sb);
+
+}
---
+++ src/DocSharp.Docx/DocxToMarkdownConverter.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Wordprocessing;
+
+namespace DocSharp.Docx;
+
+public class DocxToMarkdownConverter : DocxConverterBase
+{
+    internal override void ProcessParagraph(Paragraph paragraph, StringBuilder sb)
+    {
+        base.ProcessParagraph(paragraph, sb);
+        sb.AppendLine();
+    }
+
+    internal override void ProcessRun(Run run, StringBuilder sb)
+    {
+        var properties = run.GetFirstChild<RunProperties>();
+        bool hasText = !string.IsNullOrWhiteSpace(run.GetFirstChild<Text>()?.InnerText);
+        bool isBold, isItalic, isUnderline, isStrikethrough, isHighlight;
+        isBold = isItalic = isUnderline = isStrikethrough = isHighlight = false;
+
+        string leadingSpaces = string.Empty;
+        string trailingSpaces = string.Empty;
+
+        if (hasText)
+        {
+            string text = run.GetFirstChild<Text>()?.InnerText!;
+
+            leadingSpaces = StringHelpers.GetLeadingSpaces(text);
+            sb.Append(leadingSpaces);
+
+            // TODO: consider last child for trailing spaces
+            trailingSpaces = StringHelpers.GetTrailingSpaces(text);
+
+            isBold = properties?.Bold != null;
+            isItalic = properties?.Italic != null;
+            isUnderline = properties?.Underline != null;
+            isStrikethrough = (properties?.Strike != null || properties?.DoubleStrike != null);
+            isHighlight = (properties?.Highlight != null && properties.Highlight.Val != null && properties.Highlight.Val != HighlightColorValues.None);
+
+            if (isItalic)
+                sb.Append("*");
+
+            if (isBold)
+                sb.Append("**");
+
+            if (isStrikethrough)
+                sb.Append("~~");
+
+            if (isUnderline)
+                sb.Append("<u>");
+
+            if (isHighlight)
+                sb.Append("<mark>");
+        }
+
+        foreach (var element in run.Elements())
+        {
+            if (element is Text text)
+            {
+                ProcessText(text, sb);
+            }
+            else if (element is Picture picture)
+            {
+                ProcessPicture(picture, sb);
+            }
+        }
+
+        if (hasText)
+        {
+            if (isItalic)
+                sb.Append("*");
+
+            if (isBold)
+                sb.Append("**");
+
+            if (isStrikethrough)
+                sb.Append("~~");
+
+            if (isUnderline)
+                sb.Append("</u>");
+
+            if (isHighlight)
+                sb.Append("</mark>");
+
+            sb.Append(trailingSpaces);
+        }
+    }
+
+    internal void ProcessText(Text text, StringBuilder sb)
+    {
+        sb.Append(text.InnerText.Trim());
+    }
+
+    internal override void ProcessTable(Table table, StringBuilder sb)
+    {
+    }
+
+    internal override void ProcessHyperlink(Hyperlink hyperlink, StringBuilder sb)
+    {
+    }
+
+    internal override void ProcessPicture(Picture picture, StringBuilder sb)
+    {
+    }
+}
