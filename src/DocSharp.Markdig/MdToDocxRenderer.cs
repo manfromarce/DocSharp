eba3b282b42a985c714ecd648388e0d00f5154f7
manfredi.marceca <manfredi.marceca@outlook.it>
2024-11-28 21:00:15 +0100

    Add Markdown import project

---
+++ src/DocSharp.Markdig/MdToDocxRenderer.cs
@@ -0,0 +1,12 @@
+﻿using Markdig.Renderers;
+using Markdig.Syntax;
+
+namespace DocSharp.Markdig;
+
+public class MdToDocxRenderer : RendererBase
+{
+    public override object Render(MarkdownObject markdownObject)
+    {
+        throw new System.NotImplementedException();
+    }
+}
---
+++ src/DocSharp.Markdig/MdToRtfRenderer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Markdig.Renderers;
+
+namespace DocSharp.Markdig;
+
+public class MdToRtfRenderer : TextRendererBase<MdToRtfRenderer>
+{
+    public MdToRtfRenderer(TextWriter writer) : base(writer)
+    {
+    }
+}
