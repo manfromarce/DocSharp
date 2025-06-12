0fef3a8f524ba80b31967fdad3235154c51539e5
Manfredi Marceca <45036600+manfromarce@users.noreply.github.com>
2025-06-12 14:01:02 +0200

    Update ResourceDownloader.cs

--- src/DocSharp.Common/IO/ResourceDownloader.cs
+++ src/DocSharp.Common/IO/ResourceDownloader.cs
@@ -9,12 +9,17 @@ namespace DocSharp.IO;
 
 public static class ResourceDownloader
 {
+    private const string UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Safari/537.36 Edg/137.0.0.0";
+    private const int TimeutSeconds = 30;
+
     public static byte[]? DownloadFile(string url)
     {
         using (var client = new System.Net.Http.HttpClient())
         {
             // Fix issue with servers refusing connections from clients without a user agent
-            client.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36 Edg/131.0.0.0");
+            client.DefaultRequestHeaders.Add("User-Agent", UserAgent);
+            client.Timeout = TimeSpan.FromSeconds(TimeutSeconds);
+
             var response = client.GetAsync(url).Result;
             if (response.IsSuccessStatusCode)
             {
@@ -29,7 +34,9 @@ public static class ResourceDownloader
     {
         using (var client = new System.Net.Http.HttpClient())
         {
-            client.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36 Edg/131.0.0.0");
+            client.DefaultRequestHeaders.Add("User-Agent", UserAgent);
+            client.Timeout = TimeSpan.FromSeconds(TimeutSeconds);
+
             var response = await client.GetAsync(url);
             if (response.IsSuccessStatusCode)
             {
